1c0bc4cd8ee4bcfc00b30808349ae7baee334d66
kimseungyoon <neptuner24@loplat.com>
2023-02-06 23:40:38 +0900

    swagger 적용

--- src/main/java/com/example/airbnb_clone/controllers/User.java
+++ src/main/java/com/example/airbnb_clone/controllers/User.java
@@ -1,14 +1,17 @@
 package com.example.airbnb_clone.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name="Auth", description="권한, 인증")
 @RestController
 @RequestMapping("/user")
 public class User {
 
+    @Operation(summary="로그인", description="유저 로그인")
     @GetMapping("/login")
     public String login(){
         return "login";
